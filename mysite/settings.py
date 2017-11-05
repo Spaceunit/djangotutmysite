3e5d1728ea3091bb8fa37910c9036ab478af558f
Spaceunit <oleksiypolshchak@gmail.com>
2017-11-05 16:28:28 +0200

    upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.56.10', '192.168.56.10:8000'
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
